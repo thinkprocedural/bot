e7887b6d39b1b381a3d1cd75bc12b82fe0c6cc8b
iinfin <han@iinf.in>
2021-01-04 23:37:35 +0300

    Format + add context

--- bot/cogs/introductions/introductions.py
+++ bot/cogs/introductions/introductions.py
@@ -4,20 +4,27 @@ from discord.ext import commands
 from ..variables import *
 
 
-
 class Introductions(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if message.channel.name == "introductions" and message.author.id != self.client.user.id: # TODO: perhaps make this configurable in the environment
+        ctx = await self.client.get_context(message)
+
+        # TODO: make this configurable
+        if (
+            message.channel.name == "introductions"
+            and message.author.id != self.client.user.id
+        ):
             user = message.author
             user_roles = user.roles
 
             if len(user_roles) == 1 and user_roles[0].name == "@everyone":
-                member_role = [r for r in self.client.guilds[0].roles if r.name == "Member"][0] # TODO: perhaps make member role configurable in the environment
+                # TODO: make this configurable
+                member_role = [r for r in ctx.guild.roles if r.name == "Member"][0]
                 await user.add_roles(member_role)
+
 
 def setup(client):
     client.add_cog(Introductions(client))

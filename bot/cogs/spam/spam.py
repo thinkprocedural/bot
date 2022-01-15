e13a9523bf18cec631f7f6005b616e85553694ab
Han <han@iinf.in>
2022-01-15 22:54:36 +0000

    cleanup: comments

--- bot/cogs/spam/spam.py
+++ bot/cogs/spam/spam.py
@@ -18,7 +18,7 @@ class Spam(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         # keep resetting file contents to clear out logs
-        # so that the spam log is wowrking with "last X seconds"
+        # so that we are "tracking the last X seconds"
         while True:
             await asyncio.sleep(spam_log_file_clear_interval)
             with open(spam_log_file, "w") as spam_log:
@@ -26,15 +26,13 @@ class Spam(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
+        # ctx = await self.client.get_context(message)
         if message.author.id == self.client.user.id:
             return
-
-        ctx = await self.client.get_context(message)
 
         _timestamp = str(int(time.time()))
         _authorid = str(message.author.id)
         _channelid = str(message.channel.id)
-
         _log = _timestamp + "___" + _authorid + "___" + _channelid
 
         counter_messages = 0
@@ -46,7 +44,6 @@ class Spam(commands.Cog):
 
             for line in file.readlines():
                 log_split = line.split("___")
-                log_timestamp = log_split[0]
                 log_authorid = log_split[1]
                 log_channelid = log_split[2]
 
@@ -58,33 +55,13 @@ class Spam(commands.Cog):
                 # number of unique channels the user has sent messages in
                 counter_channels = len(set(list_channels))
 
-        # print("{}, {}, {}".format(counter_messages, counter_channels, _log))
-
         reason = "[POTENTIAL SPAM]: `{}` has sent `{}` messages in `{}` channels, within 5 seconds".format(
             message.author.name, counter_messages, counter_channels
         )
 
         if counter_channels >= 3:
-            # if the use has sent messages to more than X unique channels - in Y seconds
-            # potential spam, ban user
+            # if the user has sent messages to more than X unique channels - in Y seconds, potential spam, ban user
             await message.author.ban(reason=reason, delete_message_days=7)
-            """
-            embed = discord.Embed(
-                title="`{}`".format("[POTENTIAL SPAM]"),
-                description=reason,
-                color=color_errr,
-            )
-            await ctx.send(embed=embed)
-            """
-
-        """
-        embed = discord.Embed(
-            title="`{}`".format(counter_messages),
-            description="`{}`\n`{}`\n`{}`".format(_timestamp, _authorid, _channelid),
-            color=color_errr,
-        )
-        await ctx.send(embed=embed)
-        """
 
 
 def setup(client):
